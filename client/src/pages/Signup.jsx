6937bc10ab7600fa196a26f64a84bf8a577966f7
HamzaKing47 <hamzashahid1535@gmail.com>
2025-05-06 11:48:49 +0500

    Image Paths Changed

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -58,7 +58,7 @@ const SignUpPage = () => {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <img alt="Tech Ex" src="./public/screen.png" className="mx-auto h-20 w-auto" />
+        <img alt="Tech Ex" src="/screen.png" className="mx-auto h-20 w-auto" />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           Register your account
         </h2>
